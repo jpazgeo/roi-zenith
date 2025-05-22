f728283754c3896e023694466a131b1a5f1a5380
jpaz <jorgepaz@geotab.com>
2025-05-22 00:36:39 -0600

    Added iframe for the Marketplace recommendations component.

--- src/app/scripts/components/MarketplaceRecommendation.jsx
+++ src/app/scripts/components/MarketplaceRecommendation.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import {
-    Modal
+    Modal,
+    Waiting
 } from '@geotab/zenith'
 
 const FUEL_SAVINGS_URL = 'https://marketplace.geotab.com/category/fuel-management/?sortBy=SolutionTierAscending';
@@ -12,17 +13,31 @@ const RISK_MANAGEMENT_TITLE = 'Accident Prevention Recommendations'
 
 const MarketplaceRecommendation = ({ showMpRecommendation, toggleShowMpRecommendation, recommendationType }) => {
     const [recommendationUrl, setRecommendationUrl] = useState(0)
+    const [isLoading, setIsLoading] = useState(true);
+
     var iframeUrl = FUEL_SAVINGS_URL
     var iframeTile = FUEL_SAVINGS_TITLE
-    
+
     if (recommendationType === 1) {
         iframeUrl = RISK_MANAGEMENT_URL
         iframeTile = RISK_MANAGEMENT_TITLE
     }
 
+    useEffect(() => {
+        if (showMpRecommendation) {
+            setTimeout(() => {
+                setIsLoading(false)
+            }, 1300);
+        } else {
+            setIsLoading(true)
+        }
+    }, [showMpRecommendation])
+
     return (
-        <Modal isOpen={showMpRecommendation} onClose={() => {toggleShowMpRecommendation()}} title="Fuel Saving Recommendations">
-            <iframe src={iframeUrl} width={500} height={600}></iframe>
+        <Modal isOpen={showMpRecommendation} onClose={() => { toggleShowMpRecommendation() }} title="Fuel Saving Recommendations">
+            <Waiting isLoading={isLoading} />
+            {isLoading ? <div style={{display: 'block', width: '500px', height: '600px'}}></div> : <></>}
+            <iframe src={iframeUrl} width={500} height={600} hidden={isLoading}></iframe>
         </Modal>
     )
 }
